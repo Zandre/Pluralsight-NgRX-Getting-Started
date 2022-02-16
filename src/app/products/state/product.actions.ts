cbbb31d8d46735e0214e210f66cccf9949d7528b
Zandre Botha <botha.zandre@gmail.com>
2022-02-16 09:48:57 +0100

    Module 7: Strongly Typing Actions with Action Creators

---
+++ src/app/products/state/product.actions.ts
@@ -0,0 +1,26 @@
+import { createAction, props } from "@ngrx/store";
+
+import { Product } from "../product";
+
+export const toggleProductCode = createAction('[Product] Toggle Product Code');
+
+export const setCurrentProduct = createAction(
+    '[Product] Set Current Product',
+    props<{ product: Product }>()
+);
+
+export const clearCurrentProduct = createAction('[Product] Clear Current Product');
+
+export const initializeCurrentProduct = createAction('[Product] Initialize Current Product');
+
+export const loadProducts = createAction('[Product] Load');
+
+export const loadProductsSuccess = createAction(
+    '[Product] Load Success',
+    props<{ productd: Product[] }>()
+);
+
+export const loadProductsFailure = createAction(
+    '[Product] Load Failure',
+    props<{ error: string }>()
+);
